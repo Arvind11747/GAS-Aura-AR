2eaeb6926d1492da918ebf043290548c26d18a34
Arvind-Voidpeak <arvindgs@voidpeak.com>
2023-09-29 15:09:25 +0530

    Replication Mode
    * Set the replication mode for both AuraEnemy and AuraPlayerState.
    
    -- Replication mode determines how gameplay effects will be replicated to clients.
        --Minimal - Only replicate minimal gameplay effect info. (Useful in Multiplayer - AI Controlled)
        --Mixed - Only replicate minimal gameplay effect info to simulated proxies but full info to owners and autonomous proxies. (Useful in Multiplayer - Player Controlled).
        --Full - Replicate full gameplay info to all. (Useful in Single-player)

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -14,6 +14,8 @@ AAuraEnemy::AAuraEnemy()
 
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
 
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
