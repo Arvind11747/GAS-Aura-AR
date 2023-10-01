22c27413272e46904aa7925ef319c8e27daed4d5
Arvind <135222359+Arvind-Voidpeak@users.noreply.github.com>
2023-10-01 17:58:44 +0530

    Health Globe

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -20,6 +20,8 @@ public:
 
 	UFUNCTION()
 	virtual void OnOverlap(UPrimitiveComponent* OverlapedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
 	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
 	virtual void BeginPlay() override;
