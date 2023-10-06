47aa9182a17749eda7867efa3cda755af76bf970
Arvind-Voidpeak <arvindgs@voidpeak.com>
2023-10-06 16:01:04 +0530

    Get All Asset Tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -3,12 +3,22 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 
+
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
 }
 
-void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle GameplayEffectHandle)
+void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
+	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle GameplayEffectHandle)
 {
-	w	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("EffectApplied"));
+	FGameplayTagContainer TagContainer;
+	/*GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("EffectApplied"));*/
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO: Broadcast the tag to the WidgetController
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
 }
