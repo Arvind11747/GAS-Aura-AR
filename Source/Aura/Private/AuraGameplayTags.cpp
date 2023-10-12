424efccc2a590d62e5c4ad97f09b22aaeadf68c9
Arvind-Voidpeak <arvindgs@voidpeak.com>
2023-10-12 15:38:53 +0530

    Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Copyright Voidpeak
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces Damage taken");
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,25 @@
+// Copyright Voidpeak
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/*
+* Aura Gameplay Tags
+*
+*
+* Singleton containing native Gameplay Tags
+*/
+
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
