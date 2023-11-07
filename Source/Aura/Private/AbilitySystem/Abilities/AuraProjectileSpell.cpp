c0b85f777c5d6e1a0ea06c8a645bac5ced2fb9bd
Arvind-Voidpeak <arvindgs@voidpeak.com>
2023-11-07 11:55:04 +0530

    AuraProjectileSpell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,12 @@
+// Copyright Voidpeak
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility (C++)"), true, true, FColor::Purple, 3);
+}
