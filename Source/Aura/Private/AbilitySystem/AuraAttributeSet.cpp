42ef3b4c03349ae9b143199b909cf3247288b8b6
Arvind-Voidpeak <arvindgs@voidpeak.com>
2024-04-16 16:12:36 +0530

    Update AuraAttributeSet.cpp

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -5,6 +5,7 @@
 
 #include "GameFramework/Character.h"
 #include "GameplayEffectExtension.h"
+#include "Interface/Interaction/CombatInterface.h"
 #include "AbilitySystemBlueprintLibrary.h"
 #include "Net/UnrealNetwork.h"
 #include "AuraGameplayTags.h"
@@ -153,7 +154,15 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			SetHealth(FMath::Clamp(NewHealth, 0.f, GetMaxHealth()));
 
 			const bool bFatal = NewHealth <= 0.f;
-			if (!bFatal)
+			if (bFatal)
+			{
+				ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.Target.AvatarActor);
+				if (CombatInterface)
+				{
+					CombatInterface->Die();
+				}
+			}
+			else
 			{
 				FGameplayTagContainer TagContainer;
 				TagContainer.AddTag(FAuraGameplayTags::Get().Effects_HitReact);
